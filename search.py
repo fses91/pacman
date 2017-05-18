200a7789f82f5ec1d11888c20cefe686a5ba9d04
Florian <florian.sestak@gmail.com>
2017-05-18 15:12:49 +0200

    dfs working

--- search.py
+++ search.py
@@ -20,13 +20,13 @@ class SearchProblem:
   
   You do not need to change anything in this class, ever.
   """
-  
+
   def getStartState(self):
      """
      Returns the start state for the search problem 
      """
      util.raiseNotDefined()
-    
+
   def isGoalState(self, state):
      """
        state: Search state
@@ -58,33 +58,67 @@ class SearchProblem:
            
 
 def tinyMazeSearch(problem):
-  """
-  Returns a sequence of moves that solves tinyMaze.  For any other
-  maze, the sequence of moves will be incorrect, so only use this for tinyMaze
-  """
-  from game import Directions
-  s = Directions.SOUTH
-  w = Directions.WEST
-  return  [s,s,w,s,w,w,s,w]
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s,s,w,s,w,w,s,w]
 
 def depthFirstSearch(problem):
-  """
-  Search the deepest nodes in the search tree first
-  [2nd Edition: p 75, 3rd Edition: p 87]
-  
-  Your search algorithm needs to return a list of actions that reaches
-  the goal.  Make sure to implement a graph search algorithm 
-  [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
-  
-  To get started, you might want to try some of these simple commands to
-  understand the search problem that is being passed in:
-  
-  print "Start:", problem.getStartState()
-  print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-  print "Start's successors:", problem.getSuccessors(problem.getStartState())
-  """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    """
+    Search the deepest nodes in the search tree first
+    [2nd Edition: p 75, 3rd Edition: p 87]
+    
+    Your search algorithm needs to return a list of actions that reaches
+    the goal.  Make sure to implement a graph search algorithm 
+    [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
+    
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+    
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    actions = util.Stack()
+    visited = []
+
+    start_state = problem.getStartState()
+    visited.append(start_state)
+
+    route = generalDepthFirstSearch(problem, actions, visited, start_state)
+
+    final_route = []
+    if route != 0:
+        while not route.isEmpty():
+            final_route.append(route.pop())
+
+    return final_route[::-1]
+
+
+def generalDepthFirstSearch(problem, actions, visited, current_state):
+
+    if problem.isGoalState(current_state):
+        return actions
+
+    successors = problem.getSuccessors(current_state)
+    for node in successors[::-1]:
+        state = node[0]
+        action = node[1]
+        if state in visited:
+            continue
+
+        visited.append(state)
+        actions.push(action)
+        route = generalDepthFirstSearch(problem, actions, visited, state)
+        if route != 0:
+            return actions
+        actions.pop()
+
+    return 0
 
 def breadthFirstSearch(problem):
   """
